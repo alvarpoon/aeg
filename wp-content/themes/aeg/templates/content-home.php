531f9d0ceed8b02a7367474b1bc1d6263366fcde
Ron Chan <ksc.ron@gmail.com>
2015-11-10 19:29:14 +0800

    update Ron 10/11/2015-19:29

---
+++ wp-content/themes/aeg/templates/content-home.php
@@ -0,0 +1,22 @@
+<div class="container">
+	<div class="row">
+    	<div class="home-logo-container">
+        	<a href="<?php echo home_url(); ?>" class="logo_aeg"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/logo_aeg.png" alt="" /></a>
+        </div>
+        <div class="">
+        	<img src="<?=get_stylesheet_directory_uri()?>/assets/img/home/img_banner.png" />
+        </div>
+    </div>
+</div>
+
+<div class="container">
+	<div class="col-sm-6">
+    	jfldka sfjdsak ljfdsalk fjkaslfak slfjsdl fkldsajfklsda s fda fds asda  sda safds
+    </div>
+    <div class="col-sm-3">
+    	jfldka sfjdsak ljfdsalk fjkaslfak slfjsdl fkldsajfklsda s fda fds asda  sda safds
+    </div>
+    <div class="col-sm-3">
+    	jfldka sfjdsak ljfdsalk fjkaslfak slfjsdl fkldsajfklsda s fda fds asda  sda safds
+    </div>
+</div>
