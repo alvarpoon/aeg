6f87d91a2fe95f8f0557c03de3e106b697a7654b
alvarpoon <michiel@sedgwick-richardson.com>
2015-11-18 09:08:22 +0800

    plugin

--- wp-content/themes/aeg/templates/content-page.php
+++ wp-content/themes/aeg/templates/content-page.php
@@ -1,2 +1,10 @@
-<?php the_content(); ?>
-<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+<div class="container">
+	<div class="row">
+    	<div class="page-banner-container">
+            <h1><?=the_title();?></h1>
+        </div>
+    </div>
+    <div class="row">
+    	<?php the_content(); ?>
+    </div>
+</div>
