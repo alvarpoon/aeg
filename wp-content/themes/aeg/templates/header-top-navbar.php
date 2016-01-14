cd454efa79a36078b191c0e5c99992345a8e798c
Ron Chan <ksc.ron@gmail.com>
2016-01-14 16:48:43 +0800

    update Ron 14/01/2016-16:48

--- wp-content/themes/aeg/templates/header-top-navbar.php
+++ wp-content/themes/aeg/templates/header-top-navbar.php
@@ -9,45 +9,27 @@
       </button>
       <a href="javascript:;" class="menu-label hidden-xs hidden-sm hidden-md hidden-lg" data-toggle="collapse" data-target=".navbar-collapse">menu</a>
       <!--<a class="navbar-brand" href="<?php echo home_url(); ?>/"><img src="<?=get_stylesheet_directory_uri()?>/assets/img/logo-top.png"></a>-->
+      <a href="<?=wp_login_url(get_permalink($post->ID))?>" class="login-btn hidden-md hidden-lg">login</a> 
     </div>
   </div>
   <div class="container">
   	<div class="row">
       <div class="nav-container">
-        <!--<nav class="collapse navbar-collapse main-menu" role="navigation">
-            <?php
-                //Main menu
-                if (has_nav_menu('primary_navigation')) :
-                  wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'depth' => 0));
-                endif;
-    
-            ?>
-        </nav>-->
 		<nav class="collapse navbar-collapse main-menu navbar-right mobile-menu" role="navigation">
 			<?php
 				//Main menu
 				if (has_nav_menu('primary_navigation')) :
 				  wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav visible-xs visible-sm hidden-md hidden-lg', 'depth' => 3));
 				endif;
-	
-				/*if (has_nav_menu('top_navigation')) :
-				   wp_nav_menu(array('theme_location' => 'top_navigation', 'menu_class' => 'top-function-nav nav visible-xs visible-sm hidden-md hidden-lg','depth' => 0));
-				endif;*/
 			?>
-			<!--<label class="control-label visible-xs visible-sm hidden-md hidden-lg" for="search-textfield"><?= _e('Search')?></label>
-			<div class="search-container visible-xs visible-sm hidden-md hidden-lg clearfix">
-			  <?php get_search_form(); ?>
-			</div>-->
 		</nav>
 		<nav class="collapse navbar-collapse main-menu hidden-xs hidden-sm" role="navigation">
-			<!--<div class="container">-->
 			<?php
 			//main menu for mobile
 			if (has_nav_menu('primary_navigation')) :
 				wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav siteMainMenu','depth' => 3));
 			endif;
 			?>
-			<!--</div>-->
 		</nav>
         <?
         if(is_user_logged_in()){
